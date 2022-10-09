e4481aeaddd5be85f69ab23a44cb9639a22037d2
이가은 <rriver2@naver.com>
2022-10-09 10:02:38 +0900

    [feat] first commit

---
+++ VoiceOver Dictionary/VoiceOver_DictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceOver_DictionaryApp.swift
+//  VoiceOver Dictionary
+//
+//  Created by 이가은 on 2022/10/09.
+//
+
+import SwiftUI
+
+@main
+struct VoiceOver_DictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
