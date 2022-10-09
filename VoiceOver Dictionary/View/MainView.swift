056850f674702f1dc88bb9a97a708a0a835f7af7
이가은 <rriver2@naver.com>
2022-10-09 17:02:05 +0900

    [feat] searchbar 추가

--- VoiceOver Dictionary/View/MainView.swift
+++ VoiceOver Dictionary/View/MainView.swift
@@ -9,26 +9,36 @@ import SwiftUI
 
 
 struct MainView: View {
+    @State private var searchText: String
+    @State private var gestureList: [Gesture]
+    
+    init() {
+        self._searchText = State(initialValue: "")
+        self._gestureList = State(initialValue: Constants.GestureArray)
+    }
+    
     var body: some View {
         NavigationView {
             VStack(spacing: 0) {
                 Divider()
                 List {
                     ForEach(Constants.MainCategory.allCases, id: \.self) { mainCategory in
-                        Section(header: Text(mainCategory.rawValue).accessibility(label: Text(mainCategory.rawValue))) {
-                            let gesturesInMainCategory = Constants.GestureArray.filter {$0.mainCategory == mainCategory}
-                            ForEach(gesturesInMainCategory, id: \.self) { gesture in
-                                NavigationLink {
-                                    DetailView(gesture: gesture)
-                                } label: {
-                                    HStack(spacing: 0) {
-                                        Text(gesture.title)
-                                            .accessibility(label: Text(gesture.title))
-                                        Spacer()
-                                        Text(gesture.subTitle)
-                                            .accessibility(label: Text(gesture.subTitle))
-                                            .multilineTextAlignment(.trailing)
-                                            .foregroundColor(.gray)
+                        let gesturesInMainCategory = gestureList.filter {$0.mainCategory == mainCategory}
+                        if !gesturesInMainCategory.isEmpty {
+                            Section(header: Text(mainCategory.rawValue).accessibility(label: Text(mainCategory.rawValue))) {
+                                ForEach(gesturesInMainCategory, id: \.self) { gesture in
+                                    NavigationLink {
+                                        DetailView(gesture: gesture)
+                                    } label: {
+                                        HStack(spacing: 0) {
+                                            Text(gesture.title)
+                                                .accessibility(label: Text(gesture.title))
+                                            Spacer()
+                                            Text(gesture.subTitle)
+                                                .accessibility(label: Text(gesture.subTitle))
+                                                .multilineTextAlignment(.trailing)
+                                                .foregroundColor(.gray)
+                                        }
                                     }
                                 }
                             }
@@ -36,14 +46,23 @@ struct MainView: View {
                     }
                 }
                 .listStyle(InsetGroupedListStyle())
+                .onChange(of: searchText) { value in
+                    if searchText == "" {
+                        gestureList = Constants.GestureArray
+                    } else {
+                        gestureList = Constants.GestureArray.filter { gesture in
+                            gesture.title.contains(searchText) || gesture.subTitle.contains(searchText) || gesture.content.contains(searchText)
+                        }
+                    }
+                }
             }
             .navigationBarTitle("VoiceOver Dictionary")
-            
             .toolbar {
                 Button("Help") {
                     print("Help tapped!")
                 }
             }
+            .searchable(text: $searchText)
         }
     }
 }
