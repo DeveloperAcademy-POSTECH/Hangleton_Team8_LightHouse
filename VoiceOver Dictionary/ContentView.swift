bc32b419b7d8809e6d0b96ebd246a1d519cc7e72
이가은 <rriver2@naver.com>
2022-10-09 14:17:45 +0900

    [feat] data struct 준비

--- VoiceOver Dictionary/ContentView.swift
+++ VoiceOver Dictionary/ContentView.swift
@@ -7,20 +7,34 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MainView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                ForEach(Gesture.MainCategory.allCases, id: \.self) { mainCategory in
+                    Section(header: Text(mainCategory.rawValue)) {
+                        let gesturesInMainCategory = GestureConstance.GestureArray.filter {$0.mainCategory == mainCategory}
+                        ForEach(gesturesInMainCategory, id: \.self) { gesture in
+                            NavigationLink {
+                                DetailView(gesture: gesture)
+                            } label: {
+                                HStack(spacing: 0) {
+                                    Text(gesture.title)
+                                    Spacer()
+                                    Text(gesture.subTitle)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MainView()
     }
 }
+
--- VoiceOver Dictionary/VoiceOver_DictionaryApp.swift
+++ VoiceOver Dictionary/VoiceOver_DictionaryApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct VoiceOver_DictionaryApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
         }
     }
 }
