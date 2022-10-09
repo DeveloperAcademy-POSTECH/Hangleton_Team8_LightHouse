a8c65add705f3bcb92f87176d93ac7fdcfc237db
이가은 <rriver2@naver.com>
2022-10-09 16:16:00 +0900

    [fix] UI 수정

--- VoiceOver Dictionary/View/MainView.swift
+++ VoiceOver Dictionary/View/MainView.swift
@@ -31,7 +31,6 @@ struct MainView: View {
                                             .foregroundColor(.gray)
                                     }
                                 }
-                                .accessibilityElement(children: .contain)
                             }
                         }
                     }
