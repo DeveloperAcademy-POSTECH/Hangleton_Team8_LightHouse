1ae3ea89e12084efbd8dcc0e768b9dfe172acf1c
RuyHa <code.ruyha@gmail.com>
2022-10-11 19:38:31 +0900

    [Fix] 버그 리포트 메일을 예외 처리하여 오류를 해결 했습니다.

--- SettingView.swift
+++ SettingView.swift
@@ -12,27 +12,30 @@ struct SettingView: View {
     
     @State var result: Result<MFMailComposeResult, Error>? = nil
     @State var isShowingMailView = false
-    
+    @State private var checkingUseMail: Bool = MFMailComposeViewController.canSendMail()
     
     var body: some View {
         Form {
             NavigationLink(destination: MyWebView(urlToLoad: "https://github.com/DeveloperAcademy-POSTECH/Hangleton_Team8_LightHouse/blob/b9aef33a6cdeb487b96159b678350dc171bd4fc8/License.md").navigationTitle("라이센스")) {
                 Label("라이센스", systemImage: "doc.on.clipboard.fill")//
             }
-            Button(
-                action: {
-                    isShowingMailView.toggle()                },
-                label: {
-                    Label {
-                        Text("오류 및 문의 메일 보내기")
-                            .foregroundColor(.black)
-                    } icon : {
-                        Image(systemName: "ladybug.fill")
+            if checkingUseMail {
+                Button(
+                    action: {
+                        isShowingMailView.toggle()
+                    },
+                    label: {
+                        Label {
+                            Text("오류 및 문의 메일 보내기")
+                                .foregroundColor(.black)
+                        } icon : {
+                            Image(systemName: "ladybug.fill")
+                        }
                     }
+                )
+                .sheet(isPresented: $isShowingMailView) {
+                    MailView(isShowing: self.$isShowingMailView, result: self.$result)
                 }
-            )
-            .sheet(isPresented: $isShowingMailView) {
-                MailView(isShowing: self.$isShowingMailView, result: self.$result)
             }
         }
         .navigationTitle("설정")
